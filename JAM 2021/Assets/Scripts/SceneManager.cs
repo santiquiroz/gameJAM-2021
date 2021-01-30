59adb1e45867ff57c5d213a233503202c7b9d8c8
ZeroG234 <64936862+ZeroG234@users.noreply.github.com>
2021-01-30 18:59:39 -0500

    Update SceneManager.cs

--- JAM 2021/Assets/Scripts/SceneManager.cs
+++ JAM 2021/Assets/Scripts/SceneManager.cs
@@ -1,18 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class SceneManager : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
+public class SceneManager : MonoBehaviour{
+
+    void Start(){
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
+    void Update(){
         
     }
 }
