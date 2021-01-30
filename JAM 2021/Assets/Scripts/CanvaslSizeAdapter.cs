7c26b6e350c048673a62abe1b3700157f8847a83
Santiago Quiroz upegui <squirozu@unal.edu.co>
2021-01-30 12:40:14 -0500

    sizescript

---
+++ JAM 2021/Assets/Scripts/CanvaslSizeAdapter.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvaslSizeAdapter : MonoBehaviour{
+    
+    public 
+    void Start(){
+        
+    }
+
+    void Update(){
+        
+    }
+}
