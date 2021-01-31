f71be79577548f7f4250f66684f575d731b5fe26
Santiago Quiroz upegui <squirozu@unal.edu.co>
2021-01-31 17:41:31 -0500

    musica e intro

--- JAM 2021/Assets/Scripts/VideoController.cs
+++ JAM 2021/Assets/Scripts/VideoController.cs
@@ -6,12 +6,26 @@ using UnityEngine.Video;
 [RequireComponent(typeof(VideoPlayer))]
 public class VideoController : MonoBehaviour{
 
-    private VideoPlayer videoplayer;
+    private Camera camara;
+    private VideoPlayer videoPlayer;
+    public GameObject canvas;
+
     void Start(){
-        videoplayer = this.gameObject.GetComponent<VideoPlayer>();
+        videoPlayer = this.gameObject.GetComponent<VideoPlayer>();
+        camara = Camera.main;
+        StartCoroutine(WaitVideo(5));
     }
 
     void Update(){
         
+    }
+    IEnumerator WaitVideo(int time){
+        canvas.SetActive(false);
+        videoPlayer.Play();
+        videoPlayer.renderMode=VideoRenderMode.CameraNearPlane;
+        videoPlayer.targetCamera=camara;
+        yield return new WaitForSeconds(time);
+        videoPlayer.Stop();
+        canvas.SetActive(true);
     }
 }
